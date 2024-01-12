fdad2c4f551cc262cfeb0437af814038d7912972
kim <kim>
2024-01-12 23:35:57 +0900

    입력받은 정수의 부호(양수, 음수, 0) 출력하기

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,41 @@
+
+
+
+
+# 입력받은 정수의 부호(양수, 음수, 0) 출력하기
+
+# n = int(input('정수를 입력하세요.:'))
+
+# if n > 0:
+#     print('이 수는 양수입니다.')
+# elif n < 0:
+#     print('이 수는 음수입니다.')
+# else:
+#     print('이 수는 0입니다.')
+    
+
+
 # 세 정수를 입력받아 중앙값 구하기 1
 
-def mod3(a,b,c):
-    if a>=b:
-        if b>=c:
-            return b
-        elif a<=c:
-            return a
-        else:
-            return c
+# def mod3(a,b,c):
+#     if a>=b:
+#         if b>=c:
+#             return b
+#         elif a<=c:
+#             return a
+#         else:
+#             return c
         
-    elif a>c:
-        return a
-    elif b>c:
-        return c
-    else:
-        return b
+#     elif a>c:
+#         return a
+#     elif b>c:
+#         return c
+#     else:
+#         return b
 
 
-a = int(input('a 입력:'))
-b = int(input('b 입력:'))
-c = int(input('c 입력:'))
+# a = int(input('a 입력:'))
+# b = int(input('b 입력:'))
+# c = int(input('c 입력:'))
 
-print(f'중앙값은 {mod3(a,b,c)}')
+# print(f'중앙값은 {mod3(a,b,c)}')
