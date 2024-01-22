5725abbd82ee67a9eb949d03649aba03b6679686
김동준 <154962837+powerganking@users.noreply.github.com>
2024-01-22 22:45:23 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,10 @@
+from itertools import permutations
 import sys
+
 input = sys.stdin.readline
-
-def back_trk():
-    if m == len(a):
-        print(" ".join(map(str, a)))
-        return
-    
-    for i in range(1, n + 1):
-        if i not in a:
-            a.append(i)
-            back_trk()
-            a.pop()
-
 a = []
 n, m = map(int, input().split())
 
-back_trk()
+result = map(lambda x: ' '.join(map(str, x)), permutations(range(1, n + 1), m))
+for x in result:
+    print(x)
